431095b3af61f5076b41e17a64fa812a2cccedbd
akansha <55254687+akanshark@users.noreply.github.com>
2023-07-11 14:24:46 -0400

    23-07-11 2:24

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,6 +6,8 @@ net_total = 0
 avg_change = 0
 inc = 0
 dec = 0
+incm = ""
+decm = ""
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimeter = ',')
     print(csvreader)
@@ -16,10 +18,26 @@ with open(csvpath) as csvfile:
         net_total = net_total + row[1]
         if row[1] > inc:
             inc = row[1]
+            incm = row[0]
         if row[1] < dec:
             dec = row[1]
+            decm = row[0]
 avg_change = net_total/monthcount
-print(monthcount + "  " + net_total + "  " + avg_change + "  " + inc + "  " + dec)
+print("Total Months: " + monthcount)
+print("Total:  " + net_total)
+print("Average Change:  " + avg_change)
+print("Greatest Increase in Profits: " + incm + "(" + inc + ")")
+print("Greatest Increase in Profits: " + decm + "(" + dec + ")")
+
+
+with open('new.txt' 'w') as f:
+    f.writelines("Total Months: " + monthcount)
+    f.writelines("Total:  " + net_total)
+    f.writelines("Average Change:  " + avg_change)
+    f.writelines("Greatest Increase in Profits: " + incm + "(" + inc + ")")
+    f.writelines("Greatest Increase in Profits: " + decm + "(" + dec + ")")
+
+        
 
         
 
