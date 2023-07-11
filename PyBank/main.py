3ecd510119b039eee0216ef0e08b495ea9ef7ac6
akansha <55254687+akanshark@users.noreply.github.com>
2023-07-11 14:07:45 -0400

    23-07-11 2:07

---
+++ PyBank/main.py
@@ -0,0 +1,29 @@
+import os
+import csv
+#csvpath = os.path.join("..", "Resources", "budget_data.csv")
+monthcount = 0
+net_total = 0
+avg_change = 0
+inc = 0
+dec = 0
+with open("budget_data.csv") as csvfile:
+    csvreader = csv.reader(csvfile, delimeter = ',')
+    print(csvreader)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+    for row in csvreader:
+        monthcount+=1
+        net_total = net_total + row[1]
+        if row[1] > inc:
+            inc = row[1]
+        if row[1] < dec:
+            dec = row[1]
+avg_change = net_total/monthcount
+print(monthcount + "  " + net_total + "  " + avg_change + "  " + inc + "  " + dec)
+
+        
+
+
+
+    
+
