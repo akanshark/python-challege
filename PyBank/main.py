63c87ad999c0a268982c8a78c9bfbde3bcaf9e1b
akansha <55254687+akanshark@users.noreply.github.com>
2023-08-23 19:33:06 -0400

    08-23-2023 Push

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,47 +1,49 @@
 import os
 import csv
-csvpath = os.path.join("..", "Resources", "budget_data.csv")
+
+NAME_INDEX = 2
+CSV_PATH = os.path.join("Resources", "budget_data.csv")
+OUTPUT_PATH = os.path.join("Analysis", "budget_results.txt")
+
+os.chdir(os.path.dirname(os.path.realpath(__file__))) # use in vscode
+
 monthcount = 0
 net_total = 0
 avg_change = 0
+prev_month = 0
 inc = 0
 dec = 0
 incm = ""
 decm = ""
-with open(csvpath) as csvfile:
-    csvreader = csv.reader(csvfile, delimeter = ',')
+
+with open(CSV_PATH) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter  = ',')
     print(csvreader)
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
     for row in csvreader:
         monthcount+=1
-        net_total = net_total + row[1]
-        if row[1] > inc:
-            inc = row[1]
-            incm = row[0]
-        if row[1] < dec:
-            dec = row[1]
-            decm = row[0]
-avg_change = net_total/monthcount
-print("Total Months: " + monthcount)
-print("Total:  " + net_total)
-print("Average Change:  " + avg_change)
-print("Greatest Increase in Profits: " + incm + "(" + inc + ")")
-print("Greatest Increase in Profits: " + decm + "(" + dec + ")")
+        net_total = net_total + int(row[1])
+        change = int(row[1]) - prev_month
+        avg_change += change
+        if change > inc:
+            inc = change
+            incm = str(row[0])
+        if change < dec:
+            dec = change
+            decm = str(row[0])
+        prev_month = int(row[1])
 
+avg_change = avg_change/monthcount # error?
 
-with open('new.txt' 'w') as f:
-    f.writelines("Total Months: " + monthcount)
-    f.writelines("Total:  " + net_total)
-    f.writelines("Average Change:  " + avg_change)
-    f.writelines("Greatest Increase in Profits: " + incm + "(" + inc + ")")
-    f.writelines("Greatest Increase in Profits: " + decm + "(" + dec + ")")
+full_results = (
+    f"Total Months: {monthcount}\n"
+    f"Total: ${net_total}\n"
+    f"Average Change: ${avg_change}\n"
+    f"Greatest Increase in Profits: ${incm} ({inc})\n"
+    f"Greatest Decrease in Profits: ${decm} ({dec})\n"
+)
 
-        
-
-        
-
-
-
-    
-
+with open(OUTPUT_PATH, "w") as output_file:
+    output_file.write(full_results)
+print(full_results)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,23 +1,54 @@
 import os
 import csv
+
+NAME_INDEX = 2
+CSV_PATH = os.path.join("Resources", "election_data.csv")
+OUTPUT_PATH = os.path.join("Analysis", "election_results.txt")
+
 totalvotes = 0
 candidates = []
-candidate_vote = []
-csvpath = os.path.join("..", "Resources", "election_data.csv")
-with open(csvpath) as csvfile:
-    csvreader = csv.reader(csvfile, delimeter = ',')
+candidate_votes = {}
+winning_votes = 0
+percentages = {}
+
+
+os.chdir(os.path.dirname(os.path.realpath(__file__))) # use in vscode
+with open(CSV_PATH) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ',')
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
     for row in csvreader:
+        name = row[NAME_INDEX]
         totalvotes +=1
-        if row(3) not in candidates:
-            candidates.append(row(3))
-            candidate_vote.append(1)
+        if name not in candidate_votes:
+            candidate_votes[name] = 1 
+            candidates.append(name)    # here to fulfill assignment req
         else:
-            index = candidates.index(row(3))
-            candidate_vote[index]+=1
-for candidate in candidates:
-    index = 0
-    print(candidate + ": " + (candidate_vote[index]/totalvotes)*100 +"% (" + candidate_vote[index] + ")")
-winner_index = candidate_vote.index(max(candidate_vote))
-print("Winner: " + candidate[winner_index])
+            candidate_votes[name] += 1
+
+for name in candidate_votes:
+    votes = candidate_votes[name]
+    if votes > winning_votes:
+        winning_votes = votes
+        winning_name = name
+    percentages[name] = round(100*votes/totalvotes, 3)
+
+results_str = ''
+for candidate in candidate_votes:
+    results_str += f"{candidate}: {candidate_votes[candidate]}, {percentages[candidate]}%\n"
+
+
+full_results = (
+    "Elections\n" 
+    "---------\n"
+    f"Total Votes: {totalvotes}\n"
+    "---------\n"
+    f"{results_str}\n"
+    "---------\n"
+    f"Winner: {winning_name}\n"
+    "---------\n"
+)
+
+with open(OUTPUT_PATH, "w") as output_file:
+    output_file.write(full_results)
+print(full_results)
